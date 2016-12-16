d44e696e2b4f3c94f9e272e241be74c6acf30751
Lucian Smith <luxianosu@gmail.com>
2016-12-16 16:13:01 -0600

    Diseno +
    Fuentes, logo, alertas y validacion de campos vacios

--- www/js/controllers/registerController.js
+++ www/js/controllers/registerController.js
@@ -1,4 +1,4 @@
-app.controller('RegisterCtrl', function($scope, RegisterService) {
+app.controller('RegisterCtrl', function($scope,$ionicPopup,RegisterService,UtilService) {
   //variable objeto que se actualiza con los campos que se completan en la vista
   $scope.user = {
     username : '',
@@ -13,8 +13,33 @@ app.controller('RegisterCtrl', function($scope, RegisterService) {
   }
   $scope.register = function(){
     //control de espacios vacios
-    //control de preguntar si el usuario esta seguro
-    //servicio de registro
-    RegisterService.registerUser($scope.user);
+    if($scope.user.username != "" && $scope.user.password != ""
+      && $scope.user.lastname != "" && $scope.user.name != ""
+      && $scope.user.email != ""){
+      //control de preguntar si el usuario esta seguro
+      $scope.showConfirm("Atencion","¿Guardar cuenta?");
+    }else {
+      //servicio de registro
+      UtilService.showAlert("Atencion","Debe completar todos los campos");
+    }
   }
+
+  // A confirm dialog los popups de confirmacion no detienen el flujo de la aplicacion
+  //por lo que se utlizan en la clase controlador y no en las clases de servicio
+  $scope.showConfirm = function(title,message) {
+    var confirmPopup = $ionicPopup.confirm({
+      title: title,
+      template: message
+    });
+
+    confirmPopup.then(function(res) {
+      if(res) {
+        RegisterService.registerUser($scope.user);
+        UtilService.showAlert("Exito","Usuario Guardado Correctamente");
+        window.history.back();
+      } else {
+
+      }
+    });
+  };
 });

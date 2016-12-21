384569fdf9ba8f5e303f09eb01d28844091ec97b
Lucian Smith <lsmith@appstions.com>
2016-12-20 18:17:53 -0600

    Diseno de como van a quedar las recetas cargadas en una lista, implementacion de ion-swipe

--- www/js/controllers/dashController.js
+++ www/js/controllers/dashController.js
@@ -1,6 +1,12 @@
-app.controller('DashCtrl', function($scope) {
+app.controller('DashCtrl', function($scope,$timeout) {
+
+   $scope.listCanSwipe = true;
 
   $('#options').hide(0);
+  // $('#optionEdit').hide(0);
+  // $('#optionDelete').hide(0);
+  // $('#btnAlt').hide(0);
+
 
   $scope.addRecipe = function(){
     console.log("click");
@@ -20,5 +26,22 @@ app.controller('DashCtrl', function($scope) {
     console.log("goOptions");
     location.href = "#/account";
   };
+
+  // $scope.showEditOptions = function(){
+  //   console.log("mostrar opciones de edicion");
+  //   $('#btnPri').toggle(0);
+  //   $('#btnAlt').toggle(0);
+  //   $('#optionEdit').slideToggle('fast');
+  //   setTimeout(function() { $('#optionDelete').slideToggle('fast'); }, 500);
+  // };
+
+  $scope.editRecipe = function(){
+    console.log("editarReceta");
+  };
+
+  $scope.deleteRecipe = function(){
+    console.log("borrarReceta");
+  };
+
 
 })
